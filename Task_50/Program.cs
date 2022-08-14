6f56ce3ddf9b9bb7a81820b4005765ffb85c2170
P.I.M <z55666@yandex.ru>
2022-08-14 22:04:49 +0300

    init

---
+++ Task_50/Program.cs
@@ -0,0 +1,93 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента либо значение элемента в двумерном массиве,
+и возвращает значение либо индекс этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+4 -> такой элемент есть и его индекс 0, 1 (идеально было б найти все вхождения этого элемента)
+2, 3 -> такой элемент есть и равен 4
+5, 5 -> такой элемент отсутствует*/
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+Console.Write("Введите позицию элемента (через запятую) или значение элемента: ");
+string text = Console.ReadLine();
+string[] arrayText = text.Split(',');
+int[] mas = new int[arrayText.Length];
+string result = string.Empty;
+
+for (int i = 0; i < arrayText.Length; i++)
+{
+    mas[i] = Convert.ToInt32(arrayText[i]);
+}
+
+void PrintArrayRazm(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+
+}
+
+void FindElement(int[,] array, int[] mas)
+{
+    bool flag = false;
+    if (mas.Length == 1)
+    {
+        for (int k = 0; k < mas.Length; k++)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == mas[k])
+                    {
+                        result += $"{i}, {j}\n";
+                        flag = true;
+                    }
+                }
+            }
+        }
+        if (flag) Console.WriteLine($"Присутствует(ют) элемент(ы) в массиве с индексом(ами):\n{result}");
+        else Console.WriteLine("Такого числа в массиве нет");
+    }
+    else if (mas.Length > 1 && mas.Length < 3)
+    {
+        try
+        {
+            Console.WriteLine($"Такой элемент есть и равен: {array[mas[0], mas[1]]}");
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Такой элемент отсутствует");
+        }
+    }
+
+}
+
+FillArray(array);
+PrintArrayRazm(array);
+Console.WriteLine();
+FindElement(array, mas);
---
+++ Task_52/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+void PrintArrayRazm(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+
+}
+void Average(int[,] array)
+{
+    double result = 0;
+    double[] arrAverage = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            arrAverage[j] += array[i,j];
+        }
+    }
+    for (int i = 0; i < arrAverage.Length; i++)
+    {
+        result = Math.Round(arrAverage[i]/array.GetLength(0), 2);
+        Console.WriteLine($"Среднее арифметическое {i+1} столбца равна: {result}");
+    }
+}
+FillArray(array);
+PrintArrayRazm(array);
+Console.WriteLine();
+Average(array);
